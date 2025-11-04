5cf0ea4090040b0e933b7d68d0ec7cc48656b033
Roberto Segarelli <roby80@MacBook-Pro-di-Roberto.local>
2025-11-04 22:57:18 +0100

    fix: JSX non-null removal + render.yaml uses npm install in /web

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TestDashboard from './pages/TestDashboard'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TestDashboard />
   </React.StrictMode>
